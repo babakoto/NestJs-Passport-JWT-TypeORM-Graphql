eac23917ba94fe12c59fc4d996f3b3f75219f252
babakoto <e.tokiniaina@gmail.com>
2020-05-01 16:44:58 +0200

    clear

--- src/auth/guards/gql.guard.ts
+++ src/auth/guards/gql.guard.ts
@@ -11,14 +11,8 @@ export class GqlAuthGuard extends AuthGuard('jwt'){
 
     getRequest(context: ExecutionContext) {
         const ctx = GqlExecutionContext.create(context);
-        // req used in http queries and mutations, connection is used in websocket subscription connections, check AppModule
         const { req, connection } = ctx.getContext();
-       /* const data = (connection && connection.context && connection.context.headers)
-        ? connection.context
-        : req;*/
-    
         console.log("Data :",req.headers); 
-        // if subscriptions/webSockets, let it pass headers from connection.context to passport-jwt
         return req;
       }
 }
